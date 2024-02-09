44c0f93046e0bf5d5aab97e3c69174e3bd8fc767
Amshu Belbase <amsubelbs@gmail.com>
2024-02-09 10:54:07 +0530

    Lab 4 - Quick sort

--- QuickSort.c
+++ QuickSort.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+void printArray(int arr[], int low, int high){
+	int k;
+	printf("\nArray %d to %d : ",low, high);
+	for (k = low; k <= high; k++)
+	{
+		printf("%d ", arr[k]);
+	}
+}
 void quickSort(int arr[], int low, int high)
 {
 	int pivot = low;
@@ -21,6 +28,7 @@ void quickSort(int arr[], int low, int high)
 			arr[j] = arr[i] - arr[j];
 			arr[i] = arr[i] - arr[j];
 			printf("\nSwap %d & %d", arr[i], arr[j]);
+			printArray(arr, 0, high);
 		}
 	}
 	if (i > j && arr[pivot] > arr[j])
@@ -28,9 +36,10 @@ void quickSort(int arr[], int low, int high)
 		arr[pivot] = arr[pivot] + arr[j];
 		arr[j] = arr[pivot] - arr[j];
 		arr[pivot] = arr[pivot] - arr[j];
-		printf("\nSwap Pivot %d & %d", arr[pivot], arr[j]);
+		printf("\nSwap Pivot %d & %d :", arr[pivot], arr[j]);
+		printArray(arr, 0, high);
 	}
-	if (low != j)
+	if (low < j)
 	{
 		printf("\nSend Left Array : %d & %d", low, j);
 		quickSort(arr, low, j);
@@ -39,23 +48,21 @@ void quickSort(int arr[], int low, int high)
 		printf("\nSend Right Array : %d & %d", j+1, high);
 		quickSort(arr, j + 1, high);
 	}
-	printf("\nArray %d to %d : ",low, high);
-	for (k = low; k <= high; k++)
-	{
-		printf("%d ", arr[k]);
-	}
+	printArray(arr, low, high); 
 }
 int main()
 {
-	int i, n, key;
-	printf("Enter array size :");
-	scanf("%d", &n);
-	int arr[n];
-	for (i = 0; i < n; i++)
-	{
-		printf("Enter element at %d: ", i);
-		scanf("%d", &arr[i]);
-	}
+//	int i, n, key;
+//	printf("Enter array size :");
+//	scanf("%d", &n);
+//	int arr[n];
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("Enter element at %d: ", i);
+//		scanf("%d", &arr[i]);
+//	}
+	int arr[] = {15,5,24,8,1,3,16,10,20};
+	int n = sizeof(arr)/sizeof(arr[0]);
 	quickSort(arr, 0, n - 1);
 	return 0;
 }
